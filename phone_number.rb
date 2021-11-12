fdfeffff41b67497a16dc13467216fc5ce043444
Deepak Chauhan <rishudc119@gmail.com>
2021-11-13 01:53:32 +0530

    Model level 1 testing

---
+++ phone_number.rb
@@ -0,0 +1,9 @@
+class PhoneNumber
+  attr_reader :value
+
+  EXPECTED_NUMBER_OF_DIGITS = 10
+
+  def initialize(value)
+    @value = value.gsub(/\D/, '').split('').last(EXPECTED_NUMBER_OF_DIGITS).join
+  end
+end
---
+++ spec/phone_number_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative '../phone_number'
+RSpec.describe PhoneNumber do
+  context 'phone number contains dashes' do
+    it 'strips out the dashes' do
+      phone_number = PhoneNumber.new('555-856-8075')
+      expect(phone_number.value).to eq('5558568075')
+    end
+  end
+
+  context 'phone number contains parentheses' do
+    it 'strips out the non-numeric characters' do
+      phone_number = PhoneNumber.new('(555) 856-8075')
+      expect(phone_number.value).to eq('5558568075')
+    end
+  end
+
+  context 'phone number contains country code' do
+    it 'strips out the country code' do
+      phone_number = PhoneNumber.new('+1 555 856 8075')
+      expect(phone_number.value).to eq('5558568075')
+    end
+  end
+end
